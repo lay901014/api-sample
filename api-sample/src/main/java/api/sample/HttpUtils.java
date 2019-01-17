ba400a1be2a3a022f94c78ccb8896aecd28371c2
lulei <lulei@sjtu.edu.cn>
2019-01-17 11:00:49 +0800

    init

---
+++ api-sample/src/main/java/api/sample/HttpUtils.java
@@ -0,0 +1,51 @@
+package api.sample;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+
+public class HttpUtils {
+    
+    
+    /**
+     * http post(短信验证专用)
+     * 
+     * @return
+     */
+    public static String post(String baseUrl, String body, String authorization) {
+        try {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+
+            HttpPost httppost = new HttpPost(baseUrl);
+            httppost.setHeader("Accept", "application/json");
+            httppost.setHeader("Content-Type", "application/json;charset=utf-8");
+            httppost.setHeader("Authorization", authorization);
+            BasicHttpEntity httpEntity = new BasicHttpEntity();
+            httpEntity.setContent(new java.io.ByteArrayInputStream(body.getBytes("UTF-8")));
+            httpEntity.setContentLength(body.getBytes("UTF-8").length);
+            httppost.setEntity(httpEntity);
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            try {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    return EntityUtils.toString(entity);
+                }
+            } finally {
+                response.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+}
---
+++ api-sample/src/main/java/api/sample/SampleTest.java
@@ -0,0 +1,79 @@
+package api.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class SampleTest {
+
+	//api请求地址
+	public final static String apiEndPoint = "";
+	
+	//auth token
+	public final static String scope = "exchange_data";
+	
+	//auth client id
+	public final static String clientId = "";
+	
+	//auth secret
+	public final static String secret = "";
+	
+	//查询语句事例
+	public static String selectSql = "{ JG_JL(filter : {GH:{eq:\"${GH}\"}}){ GH CRJSZCM QSNY XH CRZW GZNR BZ JZNY SZDW ZMR SCBJ timestamp  } }";
+	
+	
+	public static void main(String[] args) {
+		
+		String token = token();
+		
+		selectSql = selectSql.replace("${GH}", "00001");
+		Map<String, String> bodyMap = new HashMap<String, String>();
+		bodyMap.put("query", selectSql);
+		
+		String response = HttpUtils.post(apiEndPoint, new Gson().toJson(bodyMap), token);
+		
+		System.out.println(response);
+	}
+	
+	/**
+	 * 客户端模式
+	 * 获取accessToken
+	 * @return
+	 */
+	public static String token() {
+		
+		OAuthClientRequest oAuthClientRequest;
+		
+		final Gson gson = new Gson();
+		
+        try {
+            oAuthClientRequest = OAuthClientRequest
+                    .tokenLocation("https://jaccount.sjtu.edu.cn/oauth2/token")
+                    .setClientId(clientId)
+                    .setClientSecret(secret)
+                    .setGrantType(GrantType.CLIENT_CREDENTIALS)
+                    .setScope(scope)
+                    .buildBodyMessage();
+            
+            OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+            OAuthJSONAccessTokenResponse oAuthResp = oAuthClient.accessToken(oAuthClientRequest);
+            JsonObject jsonObject = gson.fromJson(oAuthResp.getBody(), JsonObject.class);
+            if(jsonObject != null)
+                return jsonObject.get("access_token").getAsString();
+            
+        }catch (Exception e) {
+        	
+        }
+        
+        return null;
+	}
+	
+}
